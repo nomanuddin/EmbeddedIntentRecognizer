a2f87345a1e19debf5851e41919edc80b1049fef
Noman Ud Din <noman.uddin@live.com>
2020-10-11 14:18:02 +0200

    Intent: Project setup

---
+++ IntentApp/IntentApp.cpp
@@ -0,0 +1,12 @@
+﻿// IntentApp.cpp : Defines the entry point for the application.
+//
+
+#include "IntentApp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
